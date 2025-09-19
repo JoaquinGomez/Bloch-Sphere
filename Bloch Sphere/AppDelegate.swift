eb00881a2aad2d4caffab2ad227c5b2272ec5834
JOAQUIN ENRIQUE GOMEZ LOPEZ <joakin.93@hotmail.com>
2025-09-19 15:29:54 -0700

    Initial Commit

---
+++ Bloch Sphere/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  Bloch Sphere
+//
+//  Created by JOAQUIN ENRIQUE GOMEZ LOPEZ on 9/19/25.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
---
+++ Bloch Sphere/Bloch_SphereApp.swift
@@ -0,0 +1,19 @@
+//
+//  Bloch_SphereApp.swift
+//  Bloch Sphere
+//
+//  Created by JOAQUIN ENRIQUE GOMEZ LOPEZ on 9/19/25.
+//
+
+import SwiftUI
+
+@main
+struct Bloch_SphereApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
